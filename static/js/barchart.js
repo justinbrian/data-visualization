4489c88f8d3f6b9bcadccef38db0d5c27dc3234a
JB <justinbrian@users.noreply.github.com>
2019-11-18 11:26:17 -0800

    Deliverable.

--- static/js/barchart.js
+++ static/js/barchart.js
@@ -48,7 +48,7 @@ var svg = d3
 
 svg.call(tip);
 
-d3.tsv("data.tsv", type, function(error, data) {
+d3.tsv("static/data/data.tsv", type, function(error, data) {
    x.domain(
       data.map(function(d) {
          return d.Genre;
